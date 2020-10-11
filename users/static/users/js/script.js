384cf85ff80751459e56f8f921a61bc72df379f4
yug damor <yugdamor130296@gmail.com>
2020-10-12 01:05:23 +0530

    h

---
+++ users/static/users/js/script.js
@@ -0,0 +1,126 @@
+const initBg = (autoplay = true) => {
+    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgs = bgImgsNames.map(img => "img/" + img);
+
+    $.backstretch(bgImgs, {duration: 4000, fade: 500});
+
+    if(!autoplay) {
+      $.backstretch('pause');  
+    }    
+}
+
+const setBg = id => {
+    $.backstretch('show', id);
+}
+
+const setBgOverlay = () => {
+    const windowWidth = window.innerWidth;
+    const bgHeight = $('body').height();
+    const tmBgLeft = $('.tm-bg-left');
+
+    $('.tm-bg').height(bgHeight);
+
+    if(windowWidth > 768) {
+        tmBgLeft.css('border-left', `0`)
+                .css('border-top', `${bgHeight}px solid transparent`);                
+    } else {
+        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
+                .css('border-top', `0`);
+    }
+}
+
+$(document).ready(function () {
+    const autoplayBg = true;	// set Auto Play for Background Images
+    initBg(autoplayBg);    
+    setBgOverlay();
+
+    const bgControl = $('.tm-bg-control');            
+    bgControl.click(function() {
+        bgControl.removeClass('active');
+        $(this).addClass('active');
+        const id = $(this).data('id');                
+        setBg(id);
+    });
+
+    $(window).on("backstretch.after", function (e, instance, index) {        
+        const bgControl = $('.tm-bg-control');
+        bgControl.removeClass('active');
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
+        current.addClass('active');
+    });
+
+    $(window).resize(function() {
+        setBgOverlay();
+    });
+});
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+      if (e.type === 'keyup') {
+            if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+            label.removeClass('active highlight'); 
+            } else {
+            label.removeClass('highlight');   
+            }   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+            label.removeClass('highlight'); 
+            } 
+      else if( $this.val() !== '' ) {
+            label.addClass('highlight');
+            }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+    // Function to check Whether both passwords 
+            // is same or not. 
+            function checkPassword(form) { 
+                password1 = form.p.value; 
+                password2 = form.cp.value; 
+  
+                // If password not entered 
+                if (password1 == '') 
+                    alert ("Please enter Password"); 
+                      
+                // If confirm password not entered 
+                else if (password2 == '') 
+                    alert ("Please enter confirm password"); 
+                      
+                // If Not same return False.     
+                else if (password1 != password2) { 
+                    alert ("\nPassword did not match: Please try again...") 
+                    return false; 
+                } 
+  
+                // If same return True. 
+                else{ 
+                    alert("Password Match: Welcome") 
+                    return true; 
+                } 
+
+}
